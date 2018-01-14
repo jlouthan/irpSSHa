f8e3b3fb16bfa6feac3526229987042bbcfba046
Jennifer Louthan <jenny@uncorkedstudios.com>
2018-01-13 23:42:54 -0500

    Integrate with AbuseIPDB to get data about reports for source IPs and print in nice format

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import boto3
 from botocore.exceptions import ClientError
 import time
 
+import requests
+
 # TODO maybe move constants to separate file?
 # Name of S3 bucket and folders to store logs and queries
 BUCKET_NAME = "IP-flow-logs-561"
@@ -38,18 +40,7 @@ path = ROOT_LOGS_FOLDER + "/" + new_folder + "/" + filename;
 s3 = boto3.client('s3')
 athena = boto3.client('athena', 'us-east-1')
 
-# Call S3 to list current buckets
-response = s3.list_buckets()
-
-# Get a list of all bucket names from the response
-buckets = [bucket['Name'] for bucket in response['Buckets']]
-
-# Print out the bucket list
-# print("Bucket List: %s" % buckets)
-# print(response);
-
 # Create bucket for flow logs if it doesn't already exist
-# TODO: ensure this bucket has proper policy!
 while True:
 	try:
 		existing = s3.head_bucket(Bucket=BUCKET_NAME)
@@ -92,7 +83,7 @@ def run_query(query_string, database=None):
 				'OutputLocation': QUERY_LOCATION
 			}
 		)
-	# TODO waiters for Athena are currently being implemented in boto, until this feature is available,
+	# Waiters for Athena are currently being implemented in boto, until this feature is available,
 	# we need to. https://github.com/boto/boto3/issues/1212
 	queryId = response['QueryExecutionId']
 	# TODO move the query states into constants
@@ -110,8 +101,6 @@ def run_query(query_string, database=None):
 
 	results = athena.get_query_results(
     	QueryExecutionId=queryId
-    	# NextToken='string',
-    	# MaxResults=123
 	)
 	return results
 
@@ -159,10 +148,37 @@ print("Table exists")
 print("Querying table/bucket for potential SSH attack...")
 results = run_query(REJECTED_SSH_QUERY, DB_NAME)
 
-for row in results['ResultSet']['Rows']:
+##TODO !! Move this into separate file/class
+BASE_URL = "https://www.abuseipdb.com/"
+API_KEY = "TOtnbGPgvX0VlEh4DcY7rPBGG7fV2nkcJ4LKnZXr"
+DAYS = 60
+BRUTE_FORCE_CAT = 18
+SSH_CAT = 22
+# Given a source IP address, queries the AbuseIPDB API and returns the number of times the IP
+# has been reported in the last 60 days, and the number of those reports that were tagged with 
+# the "ssh" or "brute force" categories, or both
+def query_abuse_ip_db(source_ip):
+	payload = {'key': API_KEY, "days": DAYS}
+	res = requests.get(BASE_URL + "/check/" + source_ip + "/json", params=payload)
+	num_reports = len(res.json())
+	num_ssh_brute_force = 0
+	country = ""
+	for report in res.json():
+		if "category" not in report:
+			print(report)
+		elif BRUTE_FORCE_CAT in report["category"] or SSH_CAT in report["category"]:
+			num_ssh_brute_force += 1
+			# Seems odd, but all reports have property country with the value of 
+			# the country of the source IP
+			country = report["country"]
+	return num_reports, num_ssh_brute_force, country
+
+
+print("%-16s %-12s %-12s %-16s %-16s" % ("Source IP", "Count", "Reports", "SSH/BruteForce", "Country"))
+for row in results['ResultSet']['Rows'][1:]:
 	rowString = ""
-	for value in row['Data']:
-		rowString += value['VarCharValue']
-		rowString += "  "
-	# TODO format the printed string as needed
-	print(rowString)
+	source_ip = row['Data'][0]['VarCharValue']
+	# Query AbuseIPDB for number of times this ip has been reported
+	# Get total number of times reported, as well as # for brute force and ssh category tags
+	num_reports, num_ssh_brute_force, country = query_abuse_ip_db(source_ip)
+	print("%-16s %-12s %-12s %-16s %-16s" % (row['Data'][0]['VarCharValue'], row['Data'][1]['VarCharValue'], str(num_reports), str(num_ssh_brute_force), country))
